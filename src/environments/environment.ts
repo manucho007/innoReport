6a59b26997a7634757a2c7a11687c43accfb41c5
Manuel Rodriguez <manrodri92@gmail.com>
2019-04-10 23:57:27 +0300

    Firebase directory included

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false, 
+  firebase:{
+    apiKey: "AIzaSyDPNJaVA_xnRf3ZzHqBf-330Tr0z9JHb10",
+    authDomain: "innorent-a0799.firebaseapp.com",
+    databaseURL: "https://innorent-a0799.firebaseio.com",
+    projectId: "innorent-a0799",
+    storageBucket: "innorent-a0799.appspot.com",
+    messagingSenderId: "520895739725"
+  }
 };
 
 /*
