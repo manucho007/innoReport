e2c0f4c97539e2f54fc406b5f6174f8e063568c0
Manuel Rodriguez <manrodri92@gmail.com>
2019-04-10 23:58:39 +0300

    Custom firestore functions created

---
+++ src/app/core/firestore.service.ts
@@ -0,0 +1,84 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
+import * as firebase from 'firebase/app';
+
+// The T is a Typescript generic that allows us to use our custom interfaces
+type CollectionPredicate<T> = string | AngularFirestoreCollection<T>;
+type DocPredicate<T> = string | AngularFirestoreDocument<T>;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirestoreService {
+
+  constructor(private afs: AngularFirestore) { }
+
+  col<T>(ref: CollectionPredicate<T>, queryFn?): AngularFirestoreCollection<T> {
+    return typeof ref === 'string' ? this.afs.collection<T>(ref, queryFn) : ref;
+  }
+
+  doc<T>(ref: DocPredicate<T>): AngularFirestoreDocument<T> {
+    return typeof ref === 'string' ? this.afs.doc<T>(ref) : ref;
+  }
+
+  // ****Get Data ****** Snapshot
+  doc$<T>(ref: DocPredicate<T>): Observable<T> {
+    return this.doc(ref).snapshotChanges().pipe(map(doc => {
+      return doc.payload.data() as T;
+    }))
+  }
+
+  col$<T>(ref: CollectionPredicate<T>, queryFn?): Observable<T[]> {
+    return this.col(ref, queryFn).snapshotChanges().pipe(map(docs => {
+      return docs.map(a => a.payload.doc.data()) as T[];
+    }))
+  }
+
+
+
+  // return the collection with the ids
+  colWithIds$<T>(ref: CollectionPredicate<T>, queryFn?): Observable<any[]> {
+    return this.col(ref, queryFn).snapshotChanges().pipe(map(actions => {
+      return actions.map(a => {
+        const data = a.payload.doc.data();
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      });
+    }));
+  }
+
+  // firebase serve timestamp
+  get timestamp() {
+    return firebase.firestore.FieldValue.serverTimestamp()
+  }
+
+  //****Custom Methods
+  // Custom update method
+  update<T>(ref: DocPredicate<T>, data: any) {
+    return this.doc(ref).update({
+      ...data,
+      updatedAt: this.timestamp
+    })
+  }
+  // custom set method
+  set<T>(ref: DocPredicate<T>, data: any) {
+    const timestamp = this.timestamp
+    return this.doc(ref).set({
+      ...data,
+      updatedAt: timestamp,
+      createdAt: timestamp
+    })
+  }
+  // Custom add Method
+  add<T>(ref: CollectionPredicate<T>, data) {
+    const timestamp = this.timestamp
+    return this.col(ref).add({
+      ...data,
+      updatedAt: timestamp,
+      createdAt: timestamp
+    })
+  }
+}
