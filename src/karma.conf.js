bef2e90028863d70800932c1ef04daefd42bcf9c
Manuel Rodriguez <manrodri92@gmail.com>
2019-04-18 12:48:11 +0300

    name changed from InnoRent to InnoReport

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/innoRent'),
+      dir: require('path').join(__dirname, '../coverage/innoReport'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
