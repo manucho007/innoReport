d7feed23a6f6b2a211d737d6fdd418d12c0c6fdf
Manuel Rodriguez <manrodri92@gmail.com>
2019-04-18 15:38:35 +0300

    Functionality to create a report added

--- src/app/report/create/create.component.ts
+++ src/app/report/create/create.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FirestoreService } from "../../core/firestore.service";
+import { Report } from "../../interfaces/report";
+import { text } from '@angular/core/src/render3';
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.scss']
 })
 export class CreateComponent implements OnInit {
-
-  constructor() { }
+  report:Report={
+    title: "",
+    description:""
+  }
+  constructor(private db: FirestoreService) { }
 
   ngOnInit() {
+
   }
 
+  createRequest(){
+    this.db.add('requests',this.report);
+    this.report.title="";
+    this.report.description="";
+  }
 }
