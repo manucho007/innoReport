800f5bcc8628e0c79b78dece367e32abf913e218
Manuel Rodriguez <manrodri92@gmail.com>
2019-04-18 15:37:55 +0300

    Import to ForrmsModule added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 import { CreateComponent } from './report/create/create.component';
 import { ListComponent } from './report/list/list.component';
 import { ViewComponent } from './report/view/view.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { AngularFirestore } from "@angular/fire/firestore";
 const routes: Routes = [
   { path: '', component: ListComponent },
   { path: 'create', component: CreateComponent },
@@ -12,7 +15,13 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers:[AngularFirestore],
   exports: [RouterModule],
   declarations: [CreateComponent, ListComponent, ViewComponent]
 })
