ef14b10bc12d600e2dd5bfbad46ff9afa69cd996
Manuel Rodriguez <manrodri92@gmail.com>
2019-04-18 15:37:08 +0300

    List connected to Firestore

--- src/app/report/list/list.component.ts
+++ src/app/report/list/list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Report } from "../../interfaces/report";
+import { FirestoreService  } from "../../core/firestore.service";
+import { Observable } from "rxjs";
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
-
-  constructor() { }
+  reports: Observable<Report[]>;
+  constructor(public db: FirestoreService) { }
 
   ngOnInit() {
+    this.reports = this.db.col$('requests');
   }
 
 }
